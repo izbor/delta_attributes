4e993db132f29c66af73f7dfb5a79f9a10164e85
Oleh Novosad <oleh.novosad@gmail.com>
2012-11-20 16:01:34 +0200

    support for other sql adapters, not only mysql

--- lib/delta_attributes/mysql.rb
+++ lib/delta_attributes/mysql.rb
@@ -7,7 +7,7 @@ module Arel
       def visit_Arel_Nodes_UpdateStatement o
         [
             "UPDATE #{visit o.relation}",
-            ("SET #{o.values_changed.map { |value| visit value }.join ', '}" unless o.values.empty?),
+            ("SET #{o.values_changed.map { |value| visit value }.join ', '}" unless o.values_changed.empty?),
             ("WHERE #{o.wheres.map { |x| visit x }.join ' AND '}" unless o.wheres.empty?),
             ("ORDER BY #{o.orders.map { |x| visit x }.join(', ')}" unless o.orders.empty?),
             (visit(o.limit) if o.limit),
--- lib/delta_attributes/to_sql.rb
+++ lib/delta_attributes/to_sql.rb
@@ -16,10 +16,32 @@ module Arel
         "#{visit o.arel_node.left} = #{delta_string}"
       end
 
-      #alias_method_chain :visit_Arel_Nodes_Assignment, :deltas
-
       alias_method :visit_Arel_Nodes_Assignment_without_deltas, :visit_Arel_Nodes_Assignment
       alias_method :visit_Arel_Nodes_Assignment, :visit_Arel_Nodes_Assignment_with_deltas
+
+      def visit_Arel_Nodes_UpdateStatement o
+        if o.orders.empty? && o.limit.nil?
+          wheres = o.wheres
+        else
+          key = o.key
+          unless key
+            warn(<<-eowarn) if $VERBOSE
+(#{caller.first}) Using UpdateManager without setting UpdateManager#key is
+deprecated and support will be removed in ARel 4.0.0.  Please set the primary
+key on UpdateManager using UpdateManager#key=
+            eowarn
+            key = o.relation.primary_key
+          end
+
+          wheres = [Nodes::In.new(key, [build_subselect(key, o)])]
+        end
+
+        [
+          "UPDATE #{visit o.relation}",
+          ("SET #{o.values_changed.map { |value| visit value }.join ', '}" unless o.values_changed.empty?),
+          ("WHERE #{wheres.map { |x| visit x }.join ' AND '}" unless wheres.empty?),
+        ].compact.join ' '
+      end
     end
   end
 end
